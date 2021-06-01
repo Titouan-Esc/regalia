15b2ffa4f4111480158bccc553de0c8c6c840635
Mes Morts <titouanescorneboueu@gmail.com>
2021-06-01 22:58:53 +0200

    Fin de la journée je suis claqué

--- assets/js/app.js
+++ assets/js/app.js
@@ -160,6 +160,7 @@ function handleInput(e) {
       document.getElementById('video').style.display = 'flex';
       document.getElementById('anime').style.display = 'none';
       document.querySelector('[data-target-resolver]').style.display = 'none';
+      document.getElementById('back').style.backgroundImage = "url('..assets/images/rayon_bis.png')";
     }
     setTimeout(() => {
       afficherVideo();
